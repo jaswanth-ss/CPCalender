8e9f35597916eade212df42b0e6347ba65b8f72e
jaswanth-ss <jaswanth-ss@users.noreply.github.com>
2026-01-19 22:44:34 +0530

    Revert "Contest Page"
    This reverts commit a379abfcffcb481bb77090366a2c15dc2fee56eb.
    
    new branch

--- frontend/src/app/contests/contests.ts
+++ frontend/src/app/contests/contests.ts
@@ -1,42 +1,11 @@
-import { Component, HostListener } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { platformsArray } from '../contest.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contests',
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './contests.html',
   styleUrl: './contests.css',
 })
 export class Contests {
-  platforms : string[] = platformsArray.map(p => p.split('.')[0]);
-  filteredPlatforms : string[] = this.platforms;
-  selectedPlatforms : string[] = [];
-  searchInput:string = '';
-  dropDown:boolean = false;
-  onSearch(){
 
-  }
-  onInput(){
-    this.filteredPlatforms = this.platforms.filter(a => a.toLowerCase().includes(this.searchInput.toLocaleLowerCase()));
-  }
-  selectOption(option : string){
-    const index = this.selectedPlatforms.indexOf(option + ".com");
-    if(index === -1){
-      this.selectedPlatforms.push(option + ".com");
-    }
-    else{
-      this.selectedPlatforms.splice(index, 1);
-    }
-  }
-
-  @HostListener('document:click', ['$event'])
-  clickOutside(event: MouseEvent) {
-    const target = event.target as HTMLElement;
-    if (!target.closest('.platformSearch')) {
-      this.dropDown = false;
-      this.filteredPlatforms = this.platforms;
-    }
-  }
-  
 }
